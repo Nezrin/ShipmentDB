d605c85ccdd5cda73a0fdf6c372e5ba3ff6b3102
Nezrin <nezrin.necefzade@gmail.com>
2018-11-12 23:40:14 +0100

    initial commit

--- src/shipmentdb/Sample.java
+++ src/shipmentdb/Sample.java
@@ -111,7 +111,7 @@ public class Sample {
         {
             System.out.println("Adding Parts");
             parts.put(new PartKey("P1"),
-                      new PartValue("Nut12", "Red",
+                      new PartValue("Nut", "Red",
                                     new Weight(12.0, Weight.GRAMS),
                                     "London"));
             parts.put(new PartKey("P2"),
